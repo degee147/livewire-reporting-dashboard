1167a82afa22ed2e07d9683629d6b3620a2fabd6
Ken Waribo <degee147@gmail.com>
2025-07-07 11:45:25 +0100

    added role to users, created transactions model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role', // 'admin' or 'user'
     ];
 
     /**
@@ -63,5 +64,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
     }
 }
